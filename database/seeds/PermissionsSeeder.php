1e643788d22d0360a94836cdd774da84dda96460
Bin.josue <personhat213@gmail.com>
2020-08-01 17:08:30 -0500

    seed update hero

--- database/seeds/PermissionsSeeder.php
+++ database/seeds/PermissionsSeeder.php
@@ -35,53 +35,53 @@ class PermissionsSeeder extends Seeder
         $role4 = Role::create(['name' => 'super-admin']);
         // gets all permissions via Gate::before rule; see AuthServiceProvider
 
-        // create demo users
-        $user = Factory(App\User::class)->create([
-            'name' => 'Example asesor user',
-            'image' => 'photos/wFU44hEDR9jc5l7ynUOH3VQOlYD0jrjVtObpiI2R.png',
-            'email' => 'asesor@example.com',
-            'password' => Hash::make('asesor213'),
-        ]);
-        $user->assignRole($role1);
+        // // create demo users
+        // $user = Factory(App\User::class)->create([
+        //     'name' => 'Example asesor user',
+        //     'image' => 'photos/wFU44hEDR9jc5l7ynUOH3VQOlYD0jrjVtObpiI2R.png',
+        //     'email' => 'asesor@example.com',
+        //     'password' => Hash::make('asesor213'),
+        // ]);
+        // $user->assignRole($role1);
 
-         $user = Factory(App\User::class)->create([
-            'name' => 'Example2 asesor2 user2',
-            'image' => 'photos/wFU44hEDR9jc5l7ynUOH3VQOlYD0jrjVtObpiI2R.png',
-            'email' => 'asesor2@example.com',
-            'password' => Hash::make('asesor2213'),
-        ]);
-        $user->assignRole($role1);
+        //  $user = Factory(App\User::class)->create([
+        //     'name' => 'Example2 asesor2 user2',
+        //     'image' => 'photos/wFU44hEDR9jc5l7ynUOH3VQOlYD0jrjVtObpiI2R.png',
+        //     'email' => 'asesor2@example.com',
+        //     'password' => Hash::make('asesor2213'),
+        // ]);
+        // $user->assignRole($role1);
 
-        $user = Factory(App\User::class)->create([
-            'name' => 'Example estudiante User',
-            'image' => 'photos/wFU44hEDR9jc5l7ynUOH3VQOlYD0jrjVtObpiI2R.png',
-            'email' => 'estudiante@example.com',
-            'password' => Hash::make('estudiante213'),
-        ]);
-        $user->assignRole($role2);
+        // $user = Factory(App\User::class)->create([
+        //     'name' => 'Example estudiante User',
+        //     'image' => 'photos/wFU44hEDR9jc5l7ynUOH3VQOlYD0jrjVtObpiI2R.png',
+        //     'email' => 'estudiante@example.com',
+        //     'password' => Hash::make('estudiante213'),
+        // ]);
+        // $user->assignRole($role2);
 
-        $user = Factory(App\User::class)->create([
-            'name' => 'Example2 estudiante2 User2',
-            'image' => 'photos/wFU44hEDR9jc5l7ynUOH3VQOlYD0jrjVtObpiI2R.png',
-            'email' => 'estudiante2@example.com',
-            'password' => Hash::make('estudiante2213'),
-        ]);
-        $user->assignRole($role2);
+        // $user = Factory(App\User::class)->create([
+        //     'name' => 'Example2 estudiante2 User2',
+        //     'image' => 'photos/wFU44hEDR9jc5l7ynUOH3VQOlYD0jrjVtObpiI2R.png',
+        //     'email' => 'estudiante2@example.com',
+        //     'password' => Hash::make('estudiante2213'),
+        // ]);
+        // $user->assignRole($role2);
 
-        $user = Factory(App\User::class)->create([
-            'name' => 'Example Coornidador User',
-            'image' => 'photos/wFU44hEDR9jc5l7ynUOH3VQOlYD0jrjVtObpiI2R.png',
-            'email' => 'coordinador@example.com',
-            'password' => Hash::make('coordinador213'),
-        ]);
-        $user->assignRole($role3);
+        // $user = Factory(App\User::class)->create([
+        //     'name' => 'Example Coornidador User',
+        //     'image' => 'photos/wFU44hEDR9jc5l7ynUOH3VQOlYD0jrjVtObpiI2R.png',
+        //     'email' => 'coordinador@example.com',
+        //     'password' => Hash::make('coordinador213'),
+        // ]);
+        // $user->assignRole($role3);
 
-        $user = Factory(App\User::class)->create([
-            'name' => 'Example Super-Admin User',
-            'image' => 'photos/wFU44hEDR9jc5l7ynUOH3VQOlYD0jrjVtObpiI2R.png',
-            'email' => 'superadmin@example.com',
-            'password' => Hash::make('superadmin213'),
-        ]);
-        $user->assignRole($role4);
+        // $user = Factory(App\User::class)->create([
+        //     'name' => 'Example Super-Admin User',
+        //     'image' => 'photos/wFU44hEDR9jc5l7ynUOH3VQOlYD0jrjVtObpiI2R.png',
+        //     'email' => 'superadmin@example.com',
+        //     'password' => Hash::make('superadmin213'),
+        // ]);
+        // $user->assignRole($role4);
     }
 }
